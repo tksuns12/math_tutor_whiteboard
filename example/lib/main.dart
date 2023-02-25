e2eef9004f15e886109b5c68110df9b94b8b5e4a
tksuns12 <tksuns@gmail.com>
2023-02-25 21:07:23 +0900

    example: 녹화 모드와 라이브 모드를 선택할 수 있게 수정

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:example/whiteboard_view.dart';
 import 'package:flutter/material.dart';
-import 'package:math_tutor_whiteboard/math_tutor_whiteboard.dart';
 import 'package:math_tutor_whiteboard/types.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,20 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Math Tutor Whiteboard Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Math Tutor Whiteboard Demo'),
     );
   }
 }
@@ -52,25 +45,55 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        body: MathTutorWhiteBoard(
-      mode: WhiteboardMode.record,
-      preloadImage: const NetworkImage('https://picsum.photos/640/320'),
-      myID: 'MyID',
-      onAttemptToClose: () async {
-        print('onAttemptToClose');
-        return true;
-      },
-      onAttemptToCompleteRecording: () async {
-        print('onAttemptToCompleteRecording');
-        return true;
-      },
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => WhiteboardView(
+                      mode: WhiteboardMode.record,
+                      me: WhiteboardUser(
+                          nickname: const Uuid().v4(),
+                          micEnabled: true,
+                          chatEnabled: true,
+                          serverUid: const Uuid().v4(),
+                          id: const Uuid().v4())),
+                ));
+              },
+              child: const Text('Record Mode')),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => WhiteboardView(
+                      mode: WhiteboardMode.liveTeaching,
+                      me: WhiteboardUser(
+                          nickname: const Uuid().v4(),
+                          micEnabled: true,
+                          chatEnabled: true,
+                          serverUid: const Uuid().v4(),
+                          id: const Uuid().v4())),
+                ));
+              },
+              child: const Text('Realtime Mode'))
+        ],
+      ),
     ));
+    // return Scaffold(
+    //     body: MathTutorWhiteBoard(
+    //   mode: WhiteboardMode.record,
+    //   preloadImage: const NetworkImage('https://picsum.photos/640/320'),
+    //   myID: 'MyID',
+    //   onAttemptToClose: () async {
+    //     print('onAttemptToClose');
+    //     return true;
+    //   },
+    //   onAttemptToCompleteRecording: () async {
+    //     print('onAttemptToCompleteRecording');
+    //     return true;
+    //   },
+    // ));
   }
 }
---
+++ example/lib/whiteboard_view.dart
@@ -0,0 +1,167 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:math_tutor_whiteboard/math_tutor_whiteboard.dart';
+import 'package:math_tutor_whiteboard/types.dart';
+import 'package:uuid/uuid.dart';
+
+const kFileCode = 100;
+
+const kDrawingCode = 200;
+
+const kChatMessageCode = 300;
+
+const kServerEventCode = 400;
+
+const kViewportCode = 500;
+
+const kUserCode = 600;
+
+class WhiteboardView extends StatefulWidget {
+  final WhiteboardMode mode;
+  final WhiteboardUser me;
+  const WhiteboardView({
+    Key? key,
+    required this.mode,
+    required this.me,
+  }) : super(key: key);
+
+  @override
+  State<WhiteboardView> createState() => _WhiteboardViewState();
+}
+
+class _WhiteboardViewState extends State<WhiteboardView> {
+  Stream? inputStream;
+  StreamController? outputStream;
+  late final Future<WebSocket> webSocket;
+  @override
+  void initState() {
+    if (widget.mode == WhiteboardMode.liveTeaching) {
+      webSocket = WebSocket.connect('ws://192.168.200.106:8080/ws');
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: widget.mode != WhiteboardMode.liveTeaching
+          ? MathTutorWhiteBoard(
+              mode: WhiteboardMode.record,
+              preloadImage: const NetworkImage('https://picsum.photos/640/320'),
+              me: widget.me,
+              recordDuration: const Duration(minutes: 15),
+              onAttemptToClose: () async {
+                print('onAttemptToClose');
+                return true;
+              },
+              onAttemptToCompleteRecording: () async {
+                print('onAttemptToCompleteRecording');
+                return true;
+              },
+            )
+          : FutureBuilder<WebSocket>(
+              future: webSocket,
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data != null) {
+                  inputStream =
+                      snapshot.data!.asBroadcastStream().map((rawEvent) {
+                    final event = jsonDecode(rawEvent);
+                    final data = jsonDecode(event['data']);
+                    final senderID = event['id'];
+                    print("Input: $event");
+                    if (senderID != widget.me) {
+                      switch (event['type']) {
+                        case kChatMessageCode:
+                          return WhiteboardChatMessage.fromMap(data);
+                        case kFileCode:
+                          return File(data);
+                        case kDrawingCode:
+                          return BroadcastPaintData.fromMap(data);
+                        case kUserCode:
+                          final user = WhiteboardUser(
+                              nickname: data,
+                              micEnabled: false,
+                              chatEnabled: true,
+                              serverUid: '',
+                              id: const Uuid().v4());
+                          return UserEvent(user: user, isJoin: data);
+                        case kViewportCode:
+                          return ViewportChangeEvent.fromMap(data);
+                        default:
+                          return event;
+                      }
+                    }
+                  });
+                  outputStream = StreamController();
+                  outputStream!.stream.listen((event) {
+                    print("Output: $event");
+                    switch (event.runtimeType) {
+                      case WhiteboardChatMessage:
+                        snapshot.data!.add(jsonEncode({
+                          'id': widget.me.id,
+                          'type': kChatMessageCode,
+                          'data': (event as WhiteboardChatMessage).toJson()
+                        }));
+                        break;
+                      case File:
+                        snapshot.data!.add(jsonEncode({
+                          'id': widget.me.id,
+                          'type': kFileCode,
+                          'data': (event as File).path,
+                        }));
+                        break;
+                      case BroadcastPaintData:
+                        snapshot.data!.add(jsonEncode({
+                          'id': widget.me.id,
+                          'type': kDrawingCode,
+                          'data': (event as BroadcastPaintData).toJson()
+                        }));
+                        break;
+                      case UserEvent:
+                        snapshot.data!.add(jsonEncode({
+                          'id': widget.me.id,
+                          'type': kUserCode,
+                          'data': (event as UserEvent).user.nickname,
+                          'isEnter': (event).isJoin,
+                        }));
+                        break;
+                      case ViewportChangeEvent:
+                        snapshot.data!.add(jsonEncode({
+                          'id': widget.me.id,
+                          'type': kViewportCode,
+                          'data': (event as ViewportChangeEvent).toJson()
+                        }));
+                        break;
+                      default:
+                        snapshot.data!.add(jsonEncode(event));
+                    }
+                  });
+                  return MathTutorWhiteBoard(
+                    mode: WhiteboardMode.liveTeaching,
+                    preloadImage:
+                        const NetworkImage('https://picsum.photos/640/320'),
+                    me: widget.me,
+                    inputStream: inputStream,
+                    outputStream: outputStream,
+                    onAttemptToClose: () async {
+                      print('onAttemptToClose');
+                      return true;
+                    },
+                    onAttemptToCompleteRecording: () async {
+                      print('onAttemptToCompleteRecording');
+                      return true;
+                    },
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
+    );
+  }
+}
