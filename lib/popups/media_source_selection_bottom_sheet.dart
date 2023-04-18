b3fae53b1f1997683f6ff388b7e6e0a57604948d
tksuns12 <tksuns@gmail.com>
2023-04-18 13:30:15 +0900

    fix: 파일 선택 시 권한 플로우 강화 및 이미지 피커 라이브러리 교체

--- lib/popups/media_source_selection_bottom_sheet.dart
+++ lib/popups/media_source_selection_bottom_sheet.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:math_tutor_whiteboard/camera_capture_page.dart';
-import 'package:math_tutor_whiteboard/const.dart';
 import 'package:math_tutor_whiteboard/image_crop_page.dart';
 import 'dart:ui' as ui;
+
+import 'package:permission_handler/permission_handler.dart';
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 class MediaSourceSelectionBottomSheet extends StatelessWidget {
   final void Function(ui.Image image) onImageSelected;
@@ -26,13 +27,20 @@ class MediaSourceSelectionBottomSheet extends StatelessWidget {
             leading: const Icon(Icons.camera_alt),
             title: const Text('카메라'),
             onTap: () async {
-              final navigator = Navigator.of(context);
-              final result = await navigator.push(MaterialPageRoute(
-                builder: (context) => const CameraCapturePage(),
-              ));
-              if (result != null) {
-                onImageSelected(result);
-                navigator.pop();
+              final cameraPermission = await Permission.camera.request();
+              if (!cameraPermission.isGranted) {
+                Fluttertoast.showToast(msg: '카메라 권한이 없습니다.');
+                return;
+              }
+              if (context.mounted) {
+                final result =
+                    await Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const CameraCapturePage(),
+                ));
+                if (result != null && context.mounted) {
+                  onImageSelected(result);
+                  Navigator.of(context).pop();
+                }
               }
             },
           ),
@@ -40,19 +48,28 @@ class MediaSourceSelectionBottomSheet extends StatelessWidget {
             leading: const Icon(Icons.photo_library),
             title: const Text('갤러리'),
             onTap: () async {
-              final navigator = Navigator.of(context);
-              final pickedImage = await FilePicker.platform.pickFiles(
-                  allowMultiple: false,
-                  allowedExtensions: imageExtensions,
-                  type: FileType.custom);
-              if (pickedImage != null && pickedImage.files.isNotEmpty) {
-                final result = await navigator.push(MaterialPageRoute(
-                  builder: (context) => ImageCropPage(
-                      sourceFile: File(pickedImage.files.first.path!)),
-                ));
-                if (result != null) {
-                  onImageSelected(result);
-                  navigator.pop();
+              final permission = await AssetPicker.permissionCheck();
+              if (permission != PermissionState.authorized) {
+                Fluttertoast.showToast(msg: '갤러리 권한이 없습니다.');
+                return;
+              }
+              if (context.mounted) {
+                final pickedImages = await AssetPicker.pickAssets(context,
+                    pickerConfig: const AssetPickerConfig(
+                      maxAssets: 1,
+                      requestType: RequestType.image,
+                    ));
+                final pickedImage = await pickedImages?.first.file;
+                if (pickedImage != null && context.mounted) {
+                  final result =
+                      await Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        ImageCropPage(sourceFile: File(pickedImage.path)),
+                  ));
+                  if (result != null && context.mounted) {
+                    onImageSelected(result);
+                    Navigator.of(context).pop();
+                  }
                 }
               }
             },
